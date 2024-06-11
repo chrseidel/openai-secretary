e2d00cbba8ffb0e62b80d337ac49d6a240086979
Christoph Seidel <mail@cseidel.net>
2024-06-11 15:16:05 +0200

    viewport scale 1.0 in hope of smaller filesize

--- services/pdf.js
+++ services/pdf.js
@@ -29,7 +29,7 @@ export async function loadPdf(pdfFilePath) {
  * @returns Buffer
  */
 async function pdfPageAsImage(page) {
-  const viewport = page.getViewport({scale: 1.5})
+  const viewport = page.getViewport({scale: 1.0})
   const canvas = createCanvas(viewport.width, viewport.height)
   const ctx = canvas.getContext('2d')
 
@@ -75,7 +75,7 @@ export async function pdfToImage(inputFilePath) {
     console.log(`[PDF] processing page ${i}`)
     const page = await pdfDoc.getPage(i)
     console.log(`[PDF] loading of page ${i} done`)
-    const viewport = page.getViewport({scale: 1.5})
+    const viewport = page.getViewport({scale: 1.0})
     if (resultCanvas == null) {
       const width = viewport.width
       const height = viewport.height * pdfDoc.numPages
