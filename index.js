a8098a256ffc206ca188bbe986dba7b0dd890f1f
Christoph Seidel <mail@cseidel.net>
2024-06-11 11:07:43 +0200

    improved logging

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { watchFilesystem, onFileAdded } from "./services/filesystemWatcher.js";
-import { pdfToImage } from "./services/ocr.js"
+import { pdfToImage } from "./services/pdf.js"
 import { inferFromImage } from "./services/openaiFilename.js";
 import { storePdfInGoogleDrive } from "./services/google-drive/googleDrive.js";
 import config from "./config.js"
@@ -7,16 +7,16 @@ import { unlink } from "fs/promises"
 
 async function onNewFile(inputPdfFilePath) {
     const fileAsImage = await pdfToImage(inputPdfFilePath)
-    console.log(`inferring filename and directory for document`)
+    console.log(`[SECRETARY] inferring filename and directory for document`)
     const aiResult = await inferFromImage(fileAsImage)
 
     const newFileId = await storePdfInGoogleDrive(inputPdfFilePath, aiResult.directory, aiResult.filename)
-    console.log(`created google drive file: ${aiResult.directory}/${aiResult.filename} with ID ${newFileId}`)
+    console.log(`[SECRETARY] created google drive file: ${aiResult.directory}/${aiResult.filename} with ID ${newFileId}`)
     if (config.delete_file_after_processing) {
         console.log(`deleting input file ${inputPdfFilePath}`)
         await unlink(inputPdfFilePath)
     }
-    console.log(`processing of ${inputPdfFilePath} done\n\n\n`)
+    console.log(`[SECRETARY] processing of ${inputPdfFilePath} done\n\n\n`)
 }
 
 async function main() {
--- services/pdf.js
+++ services/pdf.js
@@ -12,15 +12,15 @@ async function loadPdf(pdfFilePath) {
   let attempt = 0
   while (attempt < config.retries.pdf_loading) {
     try {
-      console.log(`loading PDF document from ${pdfFilePath}`)
+      console.log(`[PDF] loading PDF document from ${pdfFilePath}`)
       return await pdfjs.getDocument(pdfFilePath).promise
     } catch(err) {
       attempt++
-      console.log(`[OCR] error during loading of PDF: ${err}. ${(attempt == config.retries.pdf_loading) ? "FAILED" : "retrying..."}`)
+      console.log(`[PDF] error during loading of PDF: ${err}. ${(attempt == config.retries.pdf_loading) ? "FAILED" : "retrying..."}`)
       await Sleep(500)
     }
   }
-  throw(`[OCR] Failed to load PDF data from ${pdfFilePath} after attempt number ${attempt}`)
+  throw(`[PDF] Failed to load PDF data from ${pdfFilePath} after attempt number ${attempt}`)
 }
 
 /**
@@ -31,23 +31,23 @@ async function loadPdf(pdfFilePath) {
 export async function pdfToImage(inputFilePath) {
   const pdfDoc = await loadPdf(inputFilePath)
 
-  console.log(`Loading PDF document successfull.`)
+  console.log(`[PDF] Loading PDF document successfull.`)
   let resultCanvas = null
   let resultCtx = null
 
   for (let i = 1; i <= pdfDoc.numPages; i++) {
-    console.log(`processing page ${i}`)
+    console.log(`[PDF] processing page ${i}`)
     const page = await pdfDoc.getPage(i)
-    console.log(`loading of page ${i} done`)
+    console.log(`[PDF] loading of page ${i} done`)
     const viewport = page.getViewport({scale: 1.5})
     if (resultCanvas == null) {
       const width = viewport.width
       const height = viewport.height * pdfDoc.numPages
-      console.log(`initialising result canvas. width: ${width}, heigth: ${height}`)
+      console.log(`[PDF] initialising result canvas. width: ${width}, heigth: ${height}`)
       resultCanvas = createCanvas(width, height)
       resultCtx = resultCanvas.getContext('2d')
     }
-    console.log(`rendering page into canvas`)
+    console.log(`[PDF] rendering page into canvas`)
     await page.render({
       canvasContext: resultCtx,
       viewport: viewport,
